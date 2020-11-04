3d599f93335a203ddc8802d6102b0c50d4ba9c9d
deonet <72336616+deonet@users.noreply.github.com>
2020-11-04 21:30:51 +0700

    Update eth2token.mjs

--- eth2token.mjs
+++ eth2token.mjs
@@ -1,3 +1,5 @@
+import UniswapV2Abi from "./IUniswapV2Router02.mjs";
+
 import FileSync from 'lowdb/adapters/FileSync.js';
 import lowdb from 'lowdb'
 let db =
@@ -53,11 +55,13 @@ async function what(array) {
           console.log( element , element2.addressUniq , compare1.length 
           ) ;
             
+          let action2
+
           if (compare1.length===0) {
             console.log(DateFriendly.addMinutez(7*60),'@ input');
             console.log(DateFriendly.addMinutez(10) , '@ target buy');
             let current1 = new Date().addMinutez(7*60)
-            let action2 = current1 > DateFriendly  
+            action2 = current1 > DateFriendly  
             console.log(current1,'@ current +7',action2);
             //console.log();              
             
@@ -67,28 +71,53 @@ async function what(array) {
             obj2.title = element2.title ;
             obj2.inputDtSkim = element2.inputDt ;
             
-			if(action2){
-				db2.get('posts')
-					.push( obj2 )
-					.write() ;
-			}
-				
-                break;           
+            if(action2){
+              //db2.get('posts').push( obj2 ).write() ;
+              return new Promise(resolve => {
+                  resolve(obj2);
+              });
+            }
+            break;           
+          
           }else{
             //console.log( compare1.length ) ;
           }
 
+          console.log('a', action2 )
+
       }
     return new Promise(resolve => {
       setTimeout(() => {
-        resolve('lurks');
+        resolve('lurks' );
       }, 300);
     });
   }
   
-function where() {
-    var d1 = new Date().addMinutez((60*7)+ (sleepSec/60) );
-    console.log(d1 , 'sleep')
+function contract2(params){
+
+  const toContract = 
+  '0x7a250d5630b4cf539739df2c5dacb4c659f2488d';
+  const UniswapV2ContractAddress = toContract; 
+  let daiExchangeAddress2 = toContract;
+
+  const uniswapV2Contract = 
+  new web3.eth.Contract(UniswapV2Abi, UniswapV2ContractAddress);
+
+  const path1 ="0xc778417E063141139Fce010982780140Aa0cD5Ab";
+  const WETHContractAddress= path1
+
+  const path2 = '0xF9bA5210F91D0474bd1e1DcDAeC4C58E359AaD85'; // MKR
+  const TokenContractAddress2 = path2 
+
+  const tokenToEthEncodedABI2 = uniswapV2Contract.methods
+    .swapExactETHForTokens(
+        MinimumTokens, 
+        [WETHContractAddress , TokenContractAddress2 ], addressFrom, 
+        Math.floor((Date.now() + 100000) / 1000)
+    )
+    .encodeABI({
+        from: addressFrom,
+    });
 
     return new Promise(resolve => {
       setTimeout(() => {
@@ -96,7 +125,18 @@ function where() {
       }, 1000 * (sleepSec/1) );
     });
 }
-  
+
+function where() {
+  var d1 = new Date().addMinutez((60*7)+ (sleepSec/60) );
+  console.log(d1 , 'sleep')
+
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve('in the shadows');
+    }, 1000 * (sleepSec/1) );
+  });
+}
+
   async function msg() {
     const a = await who();
 
@@ -123,6 +163,8 @@ function where() {
     obj.buyCount = buyCount
 
     const b = await what(obj);
+    const contract1 = await contract2(b);
+    
     const c = await where();
   
     console.log(`${ a } ${ b } ${ c }`);
