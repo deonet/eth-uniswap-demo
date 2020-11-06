f56f15690b559adec6f5733a6c7f52608b56297c
deonet <72336616+deonet@users.noreply.github.com>
2020-11-06 17:41:57 +0700

    Update approveExchange2.mjs

--- approveExchange2.mjs
+++ approveExchange2.mjs
@@ -1,8 +1,17 @@
-import { daiTokenAbi, daiTokenAddress, daiExchangeAddress, addressFrom } from './constants.mjs';
+import FileSync from 'lowdb/adapters/FileSync.js';
+import lowdb from 'lowdb' ;
+let db = lowdb(new FileSync('dbAppr.json'));
+db.defaults({ posts: [], }).write() ;
+
+import EthTx from 'ethereumjs-tx'
+
+import { 
+  privKey ,
+  daiTokenAbi, daiTokenAddress, daiExchangeAddress, addressFrom } from './constants.mjs';
 import { sendSignedTx, web3 } from './utils.mjs';
 
 let daiTokenAddress2 = 
-'0x1f9840a85d5af5bf1d1762f925bdaddc4201f984' ;// daiTokenAddress
+'0x1f9840a85d5af5bf1d1762f925bdaddc4201f984' ; // daiTokenAddress
 
 let daiExchangeAddress2 = 
 '0x7a250d5630b4cf539739df2c5dacb4c659f2488d' ; // daiExchangeAddress
@@ -12,25 +21,163 @@ const daiTokenContract = new web3.eth.Contract(
     daiTokenAddress2
   );
 
-  const TOKENS = web3.utils.toHex(989 * 10 ** 18); // 1 DAI
+let approveUnit=989;
+
+  const TOKENS = web3.utils.toHex(approveUnit * 10 ** 18); // 1 DAI
 
   //const approveEncodedABI = daiTokenContract.methods.approve('0x7a250d5630b4cf539739df2c5dacb4c659f2488d', TOKENS).encodeABI()
   
-  const approveEncodedABI = daiTokenContract.methods.approve(daiExchangeAddress2, TOKENS).encodeABI()
+  const approveEncodedABI = daiTokenContract.methods.approve(daiExchangeAddress2, TOKENS).encodeABI();
 
-  const sendTransaction = async () => {
-    const transactionNonce = await web3.eth.getTransactionCount(addressFrom)
+const sendSignedTx2 = (transactionObject,params) =>{
+    let transaction = new EthTx.Transaction(transactionObject, {'chain':'rinkeby'})
+    const privateKey = Buffer.from(privKey, "hex")
+    transaction.sign(privateKey)
+    const serializedEthTx = transaction.serialize().toString("hex")
+    web3.eth.sendSignedTransaction(`0x${serializedEthTx}`)
+    .on("transactionHash", async (hash) => {
+        console.log(
+            `transaction sent to the network, waiting for confirmations, ${JSON.stringify(
+                hash
+            )}`
+        );
+        let obj={};
+        obj.hash = hash ;
+        obj.addressUniq = params.addressUniq ;
+        obj.addressUniq2 = params.addressUniq2 ;
+        db.get('posts').push( obj ).write();
+    })
+    .on("receipt", (receipt) => {
+        console.log(
+            `transaction received on the network`
+        );
+        setTimeout(() => { 
+          sendTransaction() ;}, 1000 * (60*5) );
+    });
+}
+
+function getTokenAddress() {
+  let address1 = '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984';
+
+  let abc='';
+  abc = abc + "\n\r" + "what to allows? " ;
+  abc = abc + "\n\r" + "token: " + (address1) ;
+  abc = abc + "\n\r" + "" + new Date() ;       
+  console.log(abc,'');
+
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(address1);
+    }, 1000 * (5) );
+  });
+}
+
+async function getSkim(){
+  db.read();
+  
+  let dbSkim =
+'C:/Users/gold1tb/Documents/GitHub/a02/uniswap-skim/public/db2.json';
+  dbSkim = lowdb(new FileSync(dbSkim));
+    
+    dbSkim.read();
+    let tokens1 = await 
+    dbSkim.get('tokens')
+    .size()
+    .value();
+    console.log(tokens1,'token total');
+
+    let loop = 15 
+
+    for (let index = 0; index < loop; index++) {
+      const element = tokens1 - loop + index ;
+      const element2 = await
+        dbSkim.get('tokens['+(element)+']')
+          .value() ;
+      
+      const compare1 = await
+      db.get('posts')
+          .filter({addressUniq: element2.addressUniq })
+          .sortBy('addressUniq')
+          .take(50)
+          .value();
+
+      if (compare1.length===0) {
+        console.log(element2.addressUniq,'');      
+        console.log('blm');
+        return new Promise(resolve => {
+          setTimeout(() => {
+            resolve(element2);
+          }, 1000 * (5) );
+        });
+        break;
+      }
+      console.log(element2,'element2');      
+    }
+    resolve(false);
+}
+
+function getApproveEncodedABI(daiTokenAddress2) {
+  let daiTokenContract2 = new web3.eth.Contract(
+    JSON.parse(daiTokenAbi),
+    daiTokenAddress2
+  );  
+  let approveEncodedABI2 = daiTokenContract2.methods.approve(daiExchangeAddress2, TOKENS).encodeABI();
+  
+  let abc='';
+  abc = abc + "\n\r" + "approve(address spender, uint256 rawAmount)";
+  abc = abc + "\n\r" + "[0] " + (daiExchangeAddress2) ;
+  abc = abc + "\n\r" + "[1] " + (approveUnit) ;   
+  console.log(abc,'');
+
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(approveEncodedABI2);
+    }, 1000 * (5) );
+  });
+}
+
+const sendTransaction = async () => {
+  let isSendSignedTx = true ;
+  
+  let action2 = await getSkim() ;
+  console.log(action2);
+  
+  let transactionNonce = -1 ;
+  transactionNonce = await web3.eth.getTransactionCount(addressFrom) ;
+
+  daiTokenAddress2 = await getTokenAddress();
+  let approveEncodedABI3 = await getApproveEncodedABI(daiTokenAddress2);
+
     const transactionObject = {
         nonce: web3.utils.toHex(transactionNonce),
         gasLimit: web3.utils.toHex(6000000),
         gasPrice: web3.utils.toHex(10000000000),
         to: daiTokenAddress2,
         from: addressFrom,
-        data: approveEncodedABI
+        data: approveEncodedABI3
       };
 
+      if (!action2)isSendSignedTx=false;
+
+      if (transactionNonce == -1)isSendSignedTx=false;
+
+      let params1 = action2 ;
+      /**
+      {
+        addressUniq: '0xb3143c3959427fa1835758f2f985aa31d1c6863e',
+        title: 'Betfluencer',
+        inputDt: 1604645300875
+      };
+       * 
+       */
+      params1.addressUniq2 = daiTokenAddress2 ;
+
     try{
-        sendSignedTx(transactionObject)
+      if (isSendSignedTx) {
+        sendSignedTx2(transactionObject,params1);        
+        //setTimeout(() => { 
+        //sendTransaction() ;}, 1000 * (60*5) );
+      }
     }catch(err){
         console.error(err)
     }
